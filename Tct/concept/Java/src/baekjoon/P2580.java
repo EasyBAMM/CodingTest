60644064ec1e872034747bc067ab54d34008fcac
EasyBAMM <huhjb1020@naver.com>
2021-08-18 00:39:27 +0900

    08-17

--- Tct/concept/Java/src/baekjoon/P2580.java
+++ Tct/concept/Java/src/baekjoon/P2580.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -78,7 +79,7 @@ public class P2580 {
 	}
 
 	public static ArrayList<Integer> is_possible(int x, int y) {
-		ArrayList<Integer> possible = new ArrayList<Integer>(List.of(1, 2, 3, 4, 5, 6, 7, 8, 9));
+		ArrayList<Integer> possible = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
 
 		for (int i = 0; i < 9; i++) {
 			if (possible.contains(sudoku[x][i]))
---
+++ Tct/concept/Java/src/programmers/Level1완주하지못한선수.java
@@ -0,0 +1,31 @@
+package programmers;
+
+import java.util.HashMap;
+
+public class Level1완주하지못한선수 {
+	public String solution(String[] participant, String[] completion) {
+		String answer = "";
+
+		HashMap<String, Integer> people = new HashMap<>();
+
+		for (String parti : participant) {
+			if (!people.containsKey(parti)) {
+				people.put(parti, 1);
+			} else {
+				people.put(parti, people.get(parti) + 1);
+			}
+		}
+
+		for (String comple : completion) {
+			people.put(comple, people.get(comple) - 1);
+		}
+
+		for (String key : people.keySet()) {
+			if (people.get(key) == 1) {
+				answer = key;
+			}
+		}
+
+		return answer;
+	}
+}
